5ec2b1a622b17db6cd0c8bc73bc665b63b1a4e4f
Josep Maria Vila <156566553+JosepMariaVila@users.noreply.github.com>
2024-01-20 21:52:50 +0100

    Add files via upload

---
+++ getNFTs.js
@@ -0,0 +1,24 @@
+function getNet() {
+  let net;
+  net = "wss://xrplcluster.com";
+  return net;
+}
+
+async function getTokens() {
+  let net = getNet();
+  const client = new xrpl.Client(net);
+  results = "Connecting to " + net + "...";
+  document.getElementById("resultField").value = results;
+  await client.connect();
+  results += "\nConnected. Getting Ridworld NFTs...";
+  document.getElementById("resultField").value = results;
+  const nfts = await client.request({
+    method: "account_nfts",
+    account: accountField.value,
+    limit: 400,
+  });
+  console.log("\nNFTs:\n " + JSON.stringify(nfts, null, 2));
+  results += "\nNFTs:\n " + JSON.stringify(nfts, null, 2);
+  document.getElementById("resultField").value = results;
+  client.disconnect();
+}
